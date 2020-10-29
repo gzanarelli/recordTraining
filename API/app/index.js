fcba5131699389d224e82fe10f758b98e1e7120d
gzanarelli <gzanarelli@sevanova.com>
2020-10-29 13:54:28 +0100

    commit before homeOffice

--- API/app/index.js
+++ API/app/index.js
@@ -1,11 +1,22 @@
 'use strict'
 
+const os = require('os')
+const cluster = require('cluster')
+
+// if (cluster.isMaster) {
+//   const nCpus = os.cpus().length
+//   console.log('*******************')
+//   console.log('Number of cpus: ', nCpus)
+//   console.log('*******************')
+//   for (let i = 0; i < nCpus; i++) {
+//     cluster.fork()
+//   }
+// } else {
 const express = require('express')
 const app = express()
 const debug = require('debug')('recordWorkout:index')
 const glob = require('glob')
 const path = require('path')
-
 /**
  * Middleware initialization
  */
@@ -17,6 +28,7 @@ require('./start/init')(app)
 require('./libs/connect')
 
 app.get('/', (req, res, next) => {
+  console.log('this the process: ', process.pid)
   res.json({ home: 'Welcome to the home page' })
 })
 
@@ -34,7 +46,8 @@ glob
  * Errors handler
  */
 require('./start/error')(app)
-
+const pid = process.pid
 app.listen(process.env.PORT, () => {
-  console.log('listen on port ' + process.env.PORT)
+  console.log('Process: ' + pid + ', listen on port ' + process.env.PORT)
 })
+// }
